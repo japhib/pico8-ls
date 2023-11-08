891cb6844d9f96f10039c9f6367e122a3b56bf46
Ronin <6971304+TheCyberRonin@users.noreply.github.com>
2023-11-08 18:59:15 -0500

    feat: Add `region` code fold support & test files

---
+++ testfiles/code-region.lua
@@ -0,0 +1,36 @@
+-- #region With Name
+
+-- Region should fold all of this
+function test()
+  -- stupid simple function
+end
+
+function other_test()
+  -- another stupid function
+end
+
+-- #endregion With Name
+
+--#region Squished with label
+
+-- this should get folded too
+
+--#endregion Squished with label
+
+--#region
+
+-- Region without a label and no space between comment and region start and end
+
+--#endregion
+
+  -- #region Indented
+
+  -- indented regions work great too
+
+  -- #endregion
+
+-- #region
+
+-- Still collapsable
+
+-- #endregion The end region can have a label without the region having one
